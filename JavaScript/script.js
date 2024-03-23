46305c8a8973d3e1abff36c40ee15c747b2ed7c2
jackywooks <146035892+jackywooks@users.noreply.github.com>
2024-03-23 10:57:21 -0400

    Initial Build of the Home Page

---
+++ JavaScript/script.js
@@ -0,0 +1,4 @@
+// // Use JavaScript to toggle the display/hide of the hamburger menu
+// document.querySelector('.hamburger-menu').addEventListener('click', function() {
+//     document.getElementById('nav_menu').classList.toggle('nav-active');
+// });
